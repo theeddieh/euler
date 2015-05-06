ba5ad3b06221d4f0dcc2311bd9ca7bd9e1ff3737
Eddie Harrison <theeddieharrison@gmail.com>
2015-05-06 00:13:42 -0700

    mixed up problems 5 and 6

--- problem05.py
+++ problem05.py
@@ -1,17 +1,1 @@
-# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
-
-import math
-
-def main():
-	sum_squares = 0
-	square_sums = 0
-
-	for i in range (1, 101):
-		sum_squares += math.pow(i, 2)
-		square_sums += i
-	square_sums = math.pow(square_sums, 2)
-	diff = square_sums - sum_squares
-	print int(diff)
-
-if __name__ == "__main__":
-	main()
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
