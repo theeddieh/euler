dbee8d26135c1a421abdd9e28d771b13196db75e
Eddie Harrison <theeddieharrison@gmail.com>
2015-05-06 00:35:54 -0700

    started on prob 5

--- problem05.py
+++ problem05.py
@@ -1,1 +1,17 @@
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+import helpers
+
+def main():
+	value = 20
+	primes = helpers.get_primes(value)
+	print primes	
+
+	product = 1
+	for i in primes:
+		product *= i
+	print product
+
+
+if __name__ == "__main__":
+	main()
