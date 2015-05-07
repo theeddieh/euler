358b90202986ce8549e9a3d1b9fa6ee4739444c5
Eddie Harrison <theeddieharrison@gmail.com>
2015-05-06 20:02:32 -0700

    solution

---
+++ problem07.py
@@ -0,0 +1,11 @@
+# Where 2is the 1st, what is the 10,001st prime number?
+
+import helpers
+
+def main():
+
+	value = 10001
+	print helpers.get_nth_prime(value)
+
+if __name__ == "__main__":
+	main()
